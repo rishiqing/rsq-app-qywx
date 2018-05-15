bbeced7775706bac31f2b4f1143e0e7aad8af7fb
Wallace Mao <maowenqiang0752@163.com>
2018-05-15 21:45:50 +0800

    add select member ui

---
+++ src/components/demo/SelectMemberDemo.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="btn-group" >
+      <div class="btn-wrap">
+        <v-touch
+          tag="a"
+          class="weui-btn weui-btn_primary"
+          href="javascript:;"
+          @tap="openMemberSelector">
+          打开选人框
+        </v-touch>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import SelectMember from 'com/pub/SelectMember'
+
+  export default {
+    name: 'SelectMemberDemo',
+    methods: {
+      openMemberSelector () {
+        SelectMember.show({
+          memberList: this.localList,
+          selectedList: this.selectedLocalList,
+          disabledIdList: this.disabledLocalList,
+          success (selList) {
+            // var idArray = util.extractProp(selList, 'emplId')
+            // that.$store.dispatch('fetchRsqidFromUserid', {corpId: corpId, idArray: idArray})
+            //   .then(function (idMap) {
+            //     that.selectedLocalList = util.getMapValuePropArray(idMap)
+            //     that.$emit('member-changed', that.selectedLocalList)
+            //   })
+          },
+          cancel () {
+          }
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/pub/SelectMember/BaseSelectMember.vue
+++ src/components/pub/SelectMember/BaseSelectMember.vue
@@ -1,86 +1,184 @@
 <template>
-  <div>
-    <div
-      class="fixed-view"
-      style="background:#fff;z-index:999;"
-      transition="expand">
-      <div class="itm-outer">
-        <div class="itm-inner">
-          <div class="itm-title u-padding-left-40">
-            <input
-              v-model="memberName"
-              type="text"
-              placeholder="请输入名称进行搜索">
-          </div>
-          <div class="itm-icons itm-front-icons u-abs-left">
-            <i class="icon icon-search" />
-          </div>
-        </div>
+  <div class="sel-canvas">
+    <div class="sel-header">
+      <div class="sel-search-wrap">
+        <input
+          type="text"
+          class="sel-search"
+          placeholder="搜索">
+        <i class="icon2-add-circle add sel-icon-search"/>
+        <i class="icon2-add-circle add sel-icon-reset"/>
       </div>
-      <ul class="itm-lst">
-        <li
-          v-for="staff in filteredUsers"
-          :key="staff.id"
-          class="kb-item u-cf">
-          <div class="clickable itm-outer">
-            <v-touch
-              class="itm-inner"
-              @tap="toggleSelect(staff)">
-              <div class="itm-title u-padding-left-60">{{ staff.name }}</div>
-              <div class="itm-icons u-abs-left">
-                <div class="itm-icon-img-wrap">
-                  <avatar
-                    :src="staff.avatar"
-                    :username="staff.name" />
-                </div>
-                <!--<img class="itm-icon-img" :src="staff.avatar" />-->
-              </div>
-              <div class="itm-icons u-abs-right">
-                <i
-                  v-show="staff.isSelected"
-                  class="icon icon-check is-active" />
-              </div>
-            </v-touch>
-          </div>
-        </li>
-      </ul>
-      <div style="padding-bottom: 150px;" />
     </div>
-    <div
-      class=""
-      style="position: fixed;bottom:0;left:0;right:0;padding:10px 0;background:#fff;z-index:9999;">
-      <div class="itm-outer itm--trans-bg">
-        <div class="itm-inner">
-          <v-touch
-            :value="btnText"
-            tag="input"
-            class="edt-todo-submit u-full-width btnConfirm"
-            type="button"
-            @tap="confirmSelect"/>
+    <div class="sel-body">
+      <div class="sel-member-list-container">
+        <ul
+          v-if="true"
+          class="sel-member-list">
+          <li
+            v-for="member in mockList"
+            :key="member.id">
+            <div class="sel-member-info">
+              <div class="sel-member-icon">
+                <i class="icon2-add-circle add"/>
+              </div>
+              <div class="sel-member-avatar">
+                <img :src="member.avatar">
+              </div>
+              <div class="sel-member-name">
+                {{ member.name }}
+              </div>
+            </div>
+            <div class="sel-member-tag">
+              {{ member.isCreator ? '创建者' : '' }}
+            </div>
+          </li>
+        </ul>
+        <div
+          v-else
+          class="sel-member-blank">
+          <div>
+            <i class="icon2-add-circle add"/>
+          </div>
+          <p>搜索无结果</p>
         </div>
       </div>
-      <div class="itm-outer itm--trans-bg">
-        <div class="itm-inner">
-          <v-touch
-            tag="input"
-            class="btn-default u-full-width btnCancel"
-            type="button"
-            value="取消"
-            @tap="selfClose"/>
-        </div>
+    </div>
+    <div class="sel-footer">
+      <div class="sel-footer-list-container">
+        <ul class="sel-footer-list">
+          <li
+            v-for="i in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,18,19,20,21,22,23]"
+            :key="i">
+            <img src="http://shp.qpic.cn/bizmp/sfD9v8uDETrX0O6zM5Aw0nkDxHyPPc2on1Ca5qsibmtE6b5lDhvY2TA/">
+          </li>
+        </ul>
       </div>
+      <div class="sel-footer-confirm">确定(100)</div>
     </div>
   </div>
 </template>
-<style lang="scss">
-  .expand-transition {
-    transition: all .3s ease;
+<style lang="scss" scoped>
+  .sel-canvas {
+    position: fixed;
+    top: 0;right: 0;bottom: 0;left: 0;
+    z-index: 10000;
+    background: #FFFFFF;
   }
-  .expand-enter {
-    transform: translateY(100%);
+  .sel-header {
+    position: fixed;
+    box-sizing: border-box;
+    top: 0;right: 0;left:0;height: 51px;
+    z-index: 10100;
+    background: #F5F5F5;
+    padding: 8px 10px;
+    box-shadow: inset 0 0 0 0 #D9D9D9;
+    border-bottom: solid 1px #E0E0E0;
   }
-  .expand-leave {
-    transform: translateY(100%);
+  .sel-footer {
+    position: fixed;
+    box-sizing: border-box;
+    right: 0;bottom: 0;left:0;height: 51px;
+    z-index: 10100;
+    background: #FAFAFA;
+    border-top: solid 1px #D5D5D5;
+  }
+  .sel-body {
+    overflow: auto;
+    padding: 51px 0 51px;
+  }
+  .sel-search-wrap {
+    position: relative;
+    box-sizing: border-box;
+    width: 100%;height: 100%;
+    border-radius: 2px;
+    background: #FFFFFF;
+    border: solid 1px #D7DADE;
+    padding: 2px 36px 2px 24px;
+  }
+  .sel-search {
+    width: 100%;height: 100%;
+  }
+  .sel-icon-search {
+    position: absolute;top: 8px;left: 5px;
+  }
+  .sel-icon-reset {
+    position: absolute;top: 8px;right: 10px;
+  }
+  .sel-footer-list-container {
+    position: absolute;
+    left: 0;top: 0;bottom: 0;right: 92px;
+    white-space: nowrap;
+    overflow-y: auto;
+  }
+  ul.sel-footer-list {
+    height: 100%;
+    display: inline-block;
+  }
+  ul.sel-footer-list li:last-child {
+    margin-right: 10px;
+  }
+  ul.sel-footer-list li {
+    display: inline-block;
+    margin-left: 10px;
+    margin-top: 8px;
+  }
+  ul.sel-footer-list li img {
+    width: 34px;height: 34px;border-radius: 2px;
+  }
+  .sel-footer-confirm {
+    position: absolute;
+    top: 0;right: 0;height: 48px;
+    width: 92px;
+    line-height: 48px;
+    text-align: center;
+    color: #5176AB;
+    font-size: 18px;
+    background: #FAFAFA;
+  }
+  ul.sel-member-list {}
+  ul.sel-member-list li {
+    position: relative;
+    height: 57px;
+    padding-left: 15px;
+  }
+  .sel-member-info {
+    box-sizing: border-box;
+    height: 100%;
+    border-bottom: solid 1px #E3E3E3;
+    overflow: hidden;
+  }
+  .sel-member-info > * {
+    float: left;
+    height: 100%;
+    line-height: 56px;
+  }
+  .sel-member-icon {}
+  .sel-member-avatar {
+    margin: 0 10px;
+  }
+  .sel-member-avatar img {
+    width: 36px;height: 36px;margin-top: 10px;
+  }
+  .sel-member-name {
+    font-size: 16px;
+  }
+  .sel-member-tag {
+    position: absolute;
+    top: 0;right: 0;height: 100%;
+    padding-right: 15px;
+    line-height: 56px;color: #4F77AA;
+  }
+  .sel-member-blank {
+    margin: 100px auto 0;
+    color: #9B9B9B;
+    text-align: center;
+  }
+  .sel-member-blank i {
+    font-size: 76px;
+  }
+  .sel-member-blank p {
+    font-size: 14px;
   }
 </style>
 <script>
@@ -101,7 +199,27 @@
         cancel: function () {},  //  点击取消按钮的回调
         localList: [],
         memberName: '',
-        selectCount: 0
+        selectCount: 0,
+        mockList: [
+          {
+            id: 1,
+            name: '刘磊',
+            avatar: 'http://shp.qpic.cn/bizmp/sfD9v8uDETrX0O6zM5Aw0nkDxHyPPc2on1Ca5qsibmtE6b5lDhvY2TA/',
+            isCreator: false
+          },
+          {
+            id: 2,
+            name: '刘浩',
+            avatar: 'http://shp.qpic.cn/bizmp/sfD9v8uDETrX0O6zM5Aw0nkDxHyPPc2on1Ca5qsibmtE6b5lDhvY2TA/',
+            isCreator: true
+          },
+          {
+            id: 3,
+            name: '刘镇武',
+            avatar: 'http://shp.qpic.cn/bizmp/sfD9v8uDETrX0O6zM5Aw0nkDxHyPPc2on1Ca5qsibmtE6b5lDhvY2TA/',
+            isCreator: false
+          }
+        ]
       }
     },
     computed: {
